b24bb0e1d99dff34b4c9c0a78335a2529ff0a964
Anderson Rafael Da Rosa <andersondarosa.net@hotmail.com>
2020-04-21 19:52:55 -0300

    Model: Clase de objeto [Cliente] añadida.

---
+++ Cliente.java
@@ -0,0 +1,43 @@
+public class Cliente {
+
+	private int numero;
+	private String apellido;
+	private String nombre;
+
+	public Cliente() {}
+
+	public Cliente(int numero, String apellido, String nombre) {
+		this.numero = numero;
+		this.apellido = apellido;
+		this.nombre = nombre;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getApellido() {
+		return this.apellido;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNombre() {
+		return this.nombre;
+	}
+
+	@Override
+	public String toString() {
+		return apellido + " " + nombre;
+	}
+}
