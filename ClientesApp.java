50804a8e0b2951d901a865fe0c4baec8b027af80
Anderson Rafael Da Rosa <andersondarosa.net@hotmail.com>
2020-04-21 19:35:19 -0300

    Proyecto creado con clase app.

---
+++ ClientesApp.java
@@ -0,0 +1,14 @@
+class ClientesApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Listado de Clientes");
+		System.out.println("==========================\n");
+
+		cargarClientes();
+	}
+
+	static void cargarClientes() {
+	
+	}
+}
